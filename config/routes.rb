4a305654f4acc3367e5000df12a8f17f526b00ca
shimahi <kzrndsk@gmail.com>
2018-11-25 16:39:09 +0900

    記事管理ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
 	resources :articles
+
+	namespace :articles do
+	  root  'articles#index'
+	end
+
 	post "authorize" => "articles#authorize"
 	get "authorization", to: "articles#authorization", as: "authorization"
+	get "list", to: "articles#list", as: "list"
+	post "content", to: "articles#content", as: "content"
 	post "logout", to: "articles#logout", as: "logout"
+	get "figure", to: "top#figure", as: "figure"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 	root "articles#index"
+ 	root "top#index"
 end
