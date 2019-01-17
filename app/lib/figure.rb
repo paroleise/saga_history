066ee582fe1ed879a2e615f5c110b70d7cbf30c3
shimahi <kzrndsk@gmail.com>
2019-01-17 10:22:57 +0900

    Update figure.rb

--- app/lib/figure.rb
+++ app/lib/figure.rb
@@ -2,7 +2,7 @@ class Figure
   attr_accessor :name, :path, :catchcopy, :job, :birth, :death, :color, :script
 
   def initialize(name, path, catchcopy, job,birth, death, color, script)
-		@name = name
+	@name = name
     @path = path
     @catchcopy = catchcopy
     @job = job
@@ -10,6 +10,6 @@ class Figure
     @death = death
     @color = color
     @script = script
-	end
+  end
 
 end
